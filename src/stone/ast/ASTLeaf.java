5f721a460f7867678a19f6af861df52912a5afff
waikai <waikai96@gmail.com>
2016-10-20 22:14:48 -0400

    ASTree and ASTLeafr

---
+++ src/stone/ast/ASTLeaf.java
@@ -0,0 +1,32 @@
+package stone.ast;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import stone.Token;
+
+public class ASTLeaf {
+	private static ArrayList<ASTree> empty = new ArrayList<>();
+	protected Token token;
+	public ASTLeaf(Token t) {
+		token = t;
+	}
+	public ASTLeaf child(int i) {
+		throw new IndexOutOfBoundsException();
+	}
+	public int numChildren() {
+		return 0;
+	}
+	public Iterator<ASTree> children() {
+		return empty.iterator();
+	}
+	public String toString() {
+		return token.getText();
+	}
+	public String location() {
+		return "at line " + token.getLineNumber(); 
+	}
+	public Token token() {
+		return token;
+	}
+}
