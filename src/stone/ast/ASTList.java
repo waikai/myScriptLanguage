0f1f5721a06003db4a4161476b36d4390e1174cd
waikai <waikai96@gmail.com>
2016-10-20 22:24:20 -0400

    ASTList is ASTree but not the leaf

---
+++ src/stone/ast/ASTList.java
@@ -0,0 +1,38 @@
+package stone.ast;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class ASTList {
+	protected List<ASTree> children;
+	public ASTList(List<ASTree> list) {
+		// TODO Auto-generated constructor stub
+		children = list;
+	}
+	public ASTree child(int i) {
+		return children.get(i);
+	}
+	public Iterator<ASTree> children() {
+		return children.iterator();
+	}
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append('(');
+		String sep = "";
+		for (ASTree tree : children) {
+			builder.append(sep);
+			sep = " ";
+			builder.append(tree.toString());
+		}
+		return builder.append(')').toString();
+	}
+	public String location() {
+		for (ASTree t: children) {
+			String string = t.toString();
+			if (string != null) {
+				return string;
+			}
+		}
+		return null;
+	}
+}
